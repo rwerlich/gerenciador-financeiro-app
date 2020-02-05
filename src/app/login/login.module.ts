f91ba0df73686c36b6d528df9b81fe03b6e2b72f
Rodrigo Werlich <rodrigowerlich@clincard.com.br>
2020-02-05 13:51:14 -0200

    login

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,23 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
 
-import { ListPage } from './list.page';
+import { IonicModule } from '@ionic/angular';
+
+import { LoginPageRoutingModule } from './login-routing.module';
+
+import { LoginPage } from './login.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: ListPage
-      }
-    ])
+    LoginPageRoutingModule
   ],
-  declarations: [ListPage]
+  declarations: [LoginPage]
 })
-export class ListPageModule {}
+export class LoginPageModule {}
